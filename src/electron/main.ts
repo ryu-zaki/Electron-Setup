3a2975dbe6aef4db87ef19b12564da6270a311ff
ryu-zaki <jhonwellespanola4@gmail.com>
2025-08-09 18:22:30 +0800

    electron changes

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,6 +2,6 @@ import { app, BrowserWindow } from "electron";
 import path from 'path'
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
+    const mainWindow = new BrowserWindow({ width: 1068, height: 900});
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
 })
